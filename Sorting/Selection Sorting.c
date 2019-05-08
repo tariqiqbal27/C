ee76ca46ea2a00bc2b24b962cbfb9311430c1264
Tariq Iqbal <47449802+tariqiqbal27@users.noreply.github.com>
2019-05-08 21:45:26 +0530

    Update Selection Sorting.c

--- Sorting/Selection Sorting.c
+++ Sorting/Selection Sorting.c
@@ -4,7 +4,7 @@ int main()
     int min = 0,j,i,tmp;
     int array[8] = {44,32,42,11,4,7,5,1} ;
 
-    for(int i=0; i<8; i++) {
+    for(i=0; i<8; i++) {
 
         min = i;
         for(j = min; j<8; j++) {
